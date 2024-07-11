5ac7727183ff8979cdfe021cf0fa6cfdc1502b99
darkdilu <dilus1322@gmail.com>
2024-07-11 15:15:51 +0700

    updated code

--- server/models/db.js
+++ server/models/db.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose")
 require('dotenv').config()
   const connectDB=async()=>{
     try {
-        await mongoose.connect(process.env.MONGODB_URL);
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log('Connected to MongoDB Atlas');
       } catch (err) {
         console.error('Error connecting to MongoDB Atlas:', err);
